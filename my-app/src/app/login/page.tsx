c1900a28a0bd77325c0484b67d47075b9d7cbfda
muhammedanshifpoothanari <muhammedanshifpoothanari@gmail.com>
2024-07-24 14:35:19 +0530

    first commit

---
+++ my-app/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return (
+        <h1> Login Page</h1>
+    )
+}
+
+export default Page;
---
+++ my-app/src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+
+const Signup = () => {
+    return (
+        <h1> Signup Now</h1>
+    )
+}
+export default Signup;

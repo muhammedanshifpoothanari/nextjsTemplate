e2c99dcf7dcd212ffc9f71cde29c8e9fb0f46b5b
sujith surendran <sujithsurendran4433@gmail.com>
2024-07-25 11:42:00 +0530

    grid layout added

---
+++ my-app/src/app/gridTest/layout.tsx
@@ -0,0 +1,19 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body >
+      <>
+            <div className="w-full h-screen bg-gray-400 grid grid-cols-4 grid-rows-[auto_1fr_1fr] gap-0">
+                <div className="bg-green-500 text-white font-bold p-5 col-span-4">main</div>
+                <div className="bg-red-500 text-white font-bold p-5 row-span-2">side</div>
+                <div className="bg-yellow-500 text-white font-bold p-5 col-span-3 row-span-2">{children}</div>
+            </div>
+        </></body>
+    </html>
+  );
+}
+

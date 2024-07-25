7062e6fd3ba61446697cacee63361de1fefe2f1f
unknown <apmajmal52@gmail.com>
2024-07-25 16:52:26 +0530

    graph side nav

---
+++ my-app/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Card from '@/components/Card'
+
+import Prodf from '@/components/job'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Page
